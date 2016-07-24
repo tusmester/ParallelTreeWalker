6a229b28b8610a4da603f6564fcd457a1a57d09f
tusmester <tusmester@gmail.com>
2016-07-24 17:52:26 +0200

    Add FileSystemElement class for enumerating a directory structure.

---
+++ src/ParallelTreeWalker.Tests/FileSystemTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ParallelTreeWalker.Elements;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelTreeWalker.Tests
+{
+    [TestClass]
+    public class FileSystemTests
+    {
+        private readonly string ROOTNAME = "TestRoot";
+
+        [TestMethod]
+        public async Task TwoLevels_Balanced()
+        {
+            var rootDir = CreateTestStructure();
+
+            var allVisitedPath = await TestWalk(new FileSystemElement(rootDir, true), 5);
+            var actual = string.Join(Environment.NewLine, allVisitedPath);
+
+            // expected results: enumerate manually, add root dir and sort
+            var expected = string.Join(Environment.NewLine, 
+                Enumerable.Union(new List<string> { rootDir }, Directory.EnumerateFileSystemEntries(rootDir, "*", SearchOption.AllDirectories)).OrderBy(p => p));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        public async Task<string[]> TestWalk(ITreeElement root, int maxParallel)
+        {
+            var allPaths = new ConcurrentBag<string>();
+
+            await TreeWalker.WalkAsync(root, new TreeWalkerOptions
+            {
+                MaxDegreeOfParallelism = maxParallel,
+                ProcessElementAsync = (element) =>
+                {
+                    var el = element as FileSystemElement;
+
+                    Trace.WriteLine(string.Format("##PTW> {0} Path: {1}", DateTime.UtcNow.ToLongTimeString(), el.Path));
+
+                    allPaths.Add(el.Path);
+
+                    return Task.FromResult<object>(null);
+                }
+            });
+
+            return allPaths.OrderBy(p => p).ToArray();
+        }
+
+        private string CreateTestStructure()
+        {
+            var currentDir = Directory.GetCurrentDirectory();
+            var rootDir = Path.Combine(currentDir, ROOTNAME);
+
+            // cleanup
+            if (Directory.Exists(rootDir))
+                Directory.Delete(rootDir, true);
+
+            var subDirs = new string[]
+            {
+                Path.Combine(rootDir, @"F1\F11"),
+                Path.Combine(rootDir, @"F1\F12"),
+                Path.Combine(rootDir, @"F2\F21"),
+                Path.Combine(rootDir, @"F2\F22"),
+                Path.Combine(rootDir, @"F3\F31"),
+                Path.Combine(rootDir, @"F3\F32"),
+            };
+
+            foreach (var subDir in subDirs)
+            {
+                Directory.CreateDirectory(subDir);
+
+                using (var writer = File.CreateText(Path.Combine(subDir, "F1.txt")))
+                {
+                    writer.Write("TEST - " + DateTime.UtcNow);
+                }
+            }
+
+            return rootDir;
+        }
+    }
+}
---
+++ src/ParallelTreeWalker/Elements/FileSystemElement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ParallelTreeWalker.Elements
+{
+    public class FileSystemElement : ITreeElement
+    {
+        public string Path { get; private set; }
+        public bool IsContainer { get; private set; }
+
+        public IEnumerable<ITreeElement> Children
+        {
+            get
+            {
+                // enumerate directories, than files
+                return Directory.EnumerateDirectories(Path).Select(dir => new FileSystemElement(dir, true))
+                    .Concat(Directory.EnumerateFiles(Path).Select(file => new FileSystemElement(file, false)));
+            }
+        }
+
+        public FileSystemElement(string path, bool isContainer)
+        {
+            if (string.IsNullOrEmpty(path))
+                throw new ArgumentNullException(path);
+
+            Path = path;
+            IsContainer = isContainer;
+        }
+    }
+}
